6c23014fd35d0e0181d7487a289a77e7749675ce
Rafael Salinas Gonzalez <rafsalgon@gmail.com>
2024-12-01 14:41:18 +0000

    Random Card Generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,43 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const cardNumbers = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
+
+const cardSuits = ["♦", "♥", "♠", "♣"];
+
+function getRandomSuit() {
+  return cardSuits[Math.floor(Math.random() * cardSuits.length)];
+}
+
+function getRandomNumber() {
+  return cardNumbers[Math.floor(Math.random() * cardNumbers.length)];
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let suitDiv = document.getElementById("topsuit");
+  let numberDiv = document.getElementById("number");
+  let bottomsuitDiv = document.getElementById("bottomsuit");
+
+  let suit = getRandomSuit();
+  suitDiv.innerHTML = suit;
+  suitDiv.classList.add(suit);
+  bottomsuitDiv.innerHTML = suit;
+  bottomsuitDiv.classList.add(suit);
+
+  let number = getRandomNumber();
+  numberDiv.innerHTML = number;
 };
